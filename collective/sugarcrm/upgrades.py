35803728a81644f9efd63ee6f4ff3f2710944b09
toutpt <toutpt@gmail.com>
2011-01-11 15:14:19 +0000

    also apply pasplugin.sugarcrm step in upgrade_1_to_2

--- collective/sugarcrm/upgrades.py
+++ collective/sugarcrm/upgrades.py
@@ -1,8 +1,13 @@
+from Products.CMFCore.utils import getToolByName
 
 def upgrade_1_to_2(context):
     setup = getToolByName(context, "portal_setup")
     setup.runImportStepFromProfile(
         'profile-collective.sugarcrm:default',
         'controlpanel',
+        purge_old=False)
+    setup.runImportStepFromProfile(
+        'profile-collective.sugarcrm:default',
+        'pasplugin.sugarcrm',
         purge_old=False)
 
