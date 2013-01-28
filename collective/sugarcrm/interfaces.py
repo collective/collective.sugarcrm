979f90ba7c8160e186a30171f2ddc05112538f54
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-01-28 18:20:20 +0100

    pep8

--- collective/sugarcrm/interfaces.py
+++ collective/sugarcrm/interfaces.py
@@ -1,5 +1,6 @@
 from zope import interface
 
+
 class ISugarCRM(interface.Interface):
     """SugarCRM webservice API"""
 
@@ -12,14 +13,14 @@ class ISugarCRM(interface.Interface):
     def get_entry(session=None, module_name="Contacts", id=""):
         """Return an entry defined by the id with all fields"""
 
-    def search(session=None, query_string='', module='Contacts',offset=0,
+    def search(session=None, query_string='', module='Contacts', offset=0,
                            max=None):
         """Search for entry from the specified module.
         By default, search for contacts"""
-    
+
     def get_module_fields(session=None, module="Contacts"):
         """Return a list of fields description for the provided module
-        
+
         a field:
 
            name = "id"
@@ -30,15 +31,16 @@ class ISugarCRM(interface.Interface):
 
         """
 
+
 class IComplexArgFactory(interface.Interface):
     """To create complex argument, just use this utility"""
-    
+
     def create(argument_type):
         """create a complex argument for the current service"""
+
 
 class IPasswordEncryption(interface.Interface):
     """Utility to crypt your password before use it with the WebService"""
 
     def crypt(password):
         """Return the encrypted password for the current service"""
-
