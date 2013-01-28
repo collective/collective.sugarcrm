a20096f55448b3528d2785d209da169a1cb4a477
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-01-28 18:19:35 +0100

    remove initialize

--- collective/sugarcrm/__init__.py
+++ collective/sugarcrm/__init__.py
@@ -1,7 +1,3 @@
 from zope.i18nmessageid.message import MessageFactory
-  # -*- extra stuff goes here -*- 
 
 SugarCRMMessageFactory = MessageFactory("collective.sugarcrm")
-
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
